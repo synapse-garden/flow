07709b23a6b7231bc742fbf3e8afac89251849e6
Kevin Weber <mrbetis@gmail.com>
2016-02-13 13:39:31 -0500

    Add buttons with states and changing text

--- src/Tut.js
+++ src/Tut.js
@@ -18,7 +18,32 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 Hello, world! I am COMMENTBOX.
+                <GrumpButton />
             </div>
+        );
+    }
+});
+
+
+var GrumpButton = React.createClass({
+    getInitialState: function() {
+        return {numGrumps: 0, grumpText: ' Grumps'};
+    },
+    handleSubmit: function(click) {
+        click.preventDefault();
+        this.setState({
+            numGrumps: this.state.numGrumps + 1,
+            grumpText: (this.state.numGrumps == 1) ? ' Grump' : ' Grumps'
+        });
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <button className="grumpButton">
+                {this.state.numGrumps}
+                {this.state.grumpText}
+                </button>
+            </form>
         );
     }
 });
