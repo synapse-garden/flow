ba24d2b9ff903da07bb6949eb91492c72cda5736
Kevin Weber <mrbetis@gmail.com>
2016-02-24 04:41:48 -0500

    Compacted entry files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import Home from './home'
-import { Provider } from 'react-redux'
+import { connect } from 'react-redux'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={this.props.store}>
-        <Home />
-      </Provider>
-    )
+import * as actionCreators from './action-creators'
+
+class App extends React.Component {
+    render () {
+    var { state } = this.props
+    return (<div>
+        <h1>Task Lists</h1>
+        <h2>{appTime}</h2><br />
+        <a
+            onClick={() => this.clickRefreshButton()}
+            className="button no-text-select">
+            Update State Time
+        </a><br />
+    </div>)
+    }
+}
+
+const mapStateToProps = (state/*, props*/) => {
+  return {
+    state
   }
 }
+
+const ConnectedApp = connect(mapStateToProps)(App)
+
+export default ConnectedApp
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import App from './app'
+import { Provider } from 'react-redux'
 import { render } from 'react-dom'
 import createStore from './create-store'
 
-import App from './app'
-
 const store = createStore( )
 
-render(
-    <App store={store} />,
-    document.getElementById('root')
-)
+render() {
+    return (
+      <Provider store={this.props.store}>
+        <App />
+      </Provider>
+      document.getElementById('root')
+    )
+}
