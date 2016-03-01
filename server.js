718df28888d6e9ab8af3d3caf8fee34488ccefce
Bodie Solomon <binary132@gmail.com>
2016-03-01 14:57:05 -0500

    Add mock backend to Webpack server

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var mocks = require('./test/backend/server.js');
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  proxy: {
+    '/api/*': {
+      target: 'http://localhost:'+config.devPort,
+      secure: false,
+      rewrite: function(req) {
+        req.url = req.url.replace(/^\/api/, '');
+      }
+    }
+  }
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
@@ -13,3 +24,5 @@ new WebpackDevServer(webpack(config), {
 
   console.log('Listening at localhost:3000');
 });
+
+mocks.backend(config.devPort);
