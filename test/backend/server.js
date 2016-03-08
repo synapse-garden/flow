b584056078d1efc4857e0fd7a030dfe2a530017e
Kevin Weber <mrbetis@gmail.com>
2016-03-08 02:21:12 -0500

    Install jQuery and Express. Set up bare Express server

--- test/backend/server.js
+++ test/backend/server.js
@@ -1,19 +1,39 @@
-var http = require('http');
+//var http = require('http');
+//import express from 'express'
+var express = require('express');
 
-// We need a function which handles requests and send response
-function handleRequest(request, response){
-  response.end('Path Hit: ' + request.url);
+var expressServer = express()
+
+
+// Express Handler
+expressServer.get('/*', (req, res) => {
+    res.send('Express server received request: ' + req.url);
+})
+
+// Express listen
+function expressBackend(port) {
+    expressServer.listen(port, function() {
+        console.log('Express server listening on port:', port);
+    });
 }
 
-function backend(port) {
-  var server = http.createServer(handleRequest);
 
-  // start our server
-  server.listen(port, function(){
-    console.log("Mock service listening on http://localhost:%s", port);
-  });
-}
-
+// // We need a function which handles requests and send response
+// function httpHandleRequest(request, response){
+//   response.end('Path Hit: ' + request.url);
+// }
+//
+// // HTTP Backend
+// function httpBackend(port) {
+//   var server = http.createServer(httpHandleRequest);
+//
+//   // start our server
+//   server.listen(port, function(){
+//     console.log("Mock service listening on http://localhost:%s", port);
+//   });
+// }
+//
 module.exports = {
-  backend: backend
+  //backend: backend
+  backend: expressBackend
 }
